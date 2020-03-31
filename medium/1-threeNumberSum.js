9e9dab7aee965095a60837cbdaa72ed4b9fa08c6
Buford Eeds <github email address>
2020-03-30 21:32:40 -0500

    done.

---
+++ medium/1-threeNumberSum.js
@@ -0,0 +1,16 @@
+const arr = [12, 3, 1, 2, -6, 5, -8, 6]
+const target = 0
+
+const threeNumberSum = (array, targetSum) => {
+  let arrayOfTriplets = []
+
+
+
+//some crazy shit
+
+
+
+  return arrayOfTriplets
+}
+
+threeNumberSum(arr,target)
