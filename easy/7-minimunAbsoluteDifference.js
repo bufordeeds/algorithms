d68944cbcd023352a2ef94ea38b43e9807debf5a
Buford Eeds <github email address>
2020-03-30 21:32:32 -0500

    done.

---
+++ easy/7-minimunAbsoluteDifference.js
@@ -0,0 +1,17 @@
+/*
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements. 
+
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
+*/
+
+ const minimumAbsoluteDifference = (array) => {
+   let arrayOfPairs = []
+
+   for(let i = 0; i < array.length; i++) {
+     
+   }
+ }
